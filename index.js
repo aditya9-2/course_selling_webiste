40139a02477d0b940b436d6e2e88456d8a11b682
Aditya <adityabasak7031@gmail.com>
2024-09-21 20:53:50 +0530

    change the routes /user to /api/v1/user

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { userRouter } = require('./routes/user');
 const { courseRouter } = require('./routes/course');
+const { adminRouter } = require('./routes/admin');
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -9,8 +10,9 @@ app.use(express.json());
 const port = process.env.PORT || 3000;
 
 
-app.use('/user', userRouter);
-app.use('/course', courseRouter)
+app.use('/api/v1/user', userRouter);
+app.use('/api/v1/admin', adminRouter);
+app.use('/api/v1/course', courseRouter);
 
 
 
