b643e617ad2783d38ac951e966fbed844dcb150c
Aditya <adityabasak7031@gmail.com>
2024-09-21 21:28:40 +0530

    modify the course router

--- routes/admin.js
+++ routes/admin.js
@@ -19,19 +19,19 @@ adminRouter.post('/signin', (req, res) => {
 
 });
 
-adminRouter.post("/course", function (req, res) {
+adminRouter.post("/", function (req, res) {
     res.json({
         message: "signup endpoint"
     })
 })
 
-adminRouter.put("/course", function (req, res) {
+adminRouter.put("/", function (req, res) {
     res.json({
         message: "signup endpoint"
     })
 })
 
-adminRouter.get("/course/bulk", function (req, res) {
+adminRouter.get("/bulk", function (req, res) {
     res.json({
         message: "signup endpoint"
     })
