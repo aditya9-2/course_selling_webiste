dcee7e84f0bf9dfea872a0baa90595649c102d9d
Aditya <adityabasak7031@gmail.com>
2024-09-21 22:04:24 +0530

    add try-catch for mongoose connection

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require('express');
-const mongoose = require('mongoose')
-const { userRouter } = require('./routes/user');
-const { courseRouter } = require('./routes/course');
-const { adminRouter } = require('./routes/admin');
-const dotenv = require('dotenv');
-dotenv.config();
+const mongoose = require('mongoose');
 
 const app = express();
 app.use(express.json());
+
+const { userRouter } = require('./routes/user');
+const { courseRouter } = require('./routes/course');
+const { adminRouter } = require('./routes/admin');
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+
 const port = process.env.PORT || 3000;
-const dburl = process.env.DB_URL
+const dburl = process.env.DB_URL;
 
 
 app.use('/api/v1/user', userRouter);
@@ -18,9 +22,15 @@ app.use('/api/v1/course', courseRouter);
 
 const main = async () => {
 
-    await mongoose.connect(dburl)
-    app.listen(port, () => {
-        console.log(`listening`);
-    });
+    try {
+        await mongoose.connect(dburl);
+        console.log('Connected to MongoDB');
+        app.listen(port, () => {
+            console.log(`listening`);
+        });
+    } catch (err) {
+        console.error('Error connecting to MongoDB', err);
+    }
+
 }
 main();
