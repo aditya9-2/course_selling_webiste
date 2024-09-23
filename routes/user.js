42057fd5ca93bdae0f10d63ad023869f5f4df6c7
Aditya <adityabasak7031@gmail.com>
2024-09-23 09:40:02 +0530

    add authentication for user

--- routes/user.js
+++ routes/user.js
@@ -1,25 +1,108 @@
 const { Router } = require('express');
 const userRouter = Router();
-
 const { userModel } = require('../db');
+const { z } = require('zod')
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
 
 //signup
-userRouter.post('/signup', (req, res) => {
+userRouter.post('/signup', async (req, res) => {
 
-    res.json({
-        message: "signup endpoint"
+    const requiredBody = z.object({
+        email: z.string().min(3).max(100).email(),
+        password: z.string().min(3).max(30),
+        firstName: z.string().min(3).max(100),
+        lastName: z.string().min(3).max(100),
     });
 
+    try {
+        const parseDataWithSuccess = requiredBody.safeParse(req.body);
 
+        if (!parseDataWithSuccess.success) {
+            return res.status(400).json({
+                message: "Incorrect Format!!",
+                error: parseDataWithSuccess.error,
+            });
+        }
+
+        const { email, password, firstName, lastName } = req.body;
+
+        // Check if the user with the given email already exists
+        const existingUser = await userModel.findOne({ email });
+
+        if (existingUser) {
+            return res.status(409).json({
+                message: "User already exists",
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 5);
+
+        await userModel.create({
+            email: email,
+            password: hashedPassword,
+            firstName: firstName,
+            lastName: lastName,
+        });
+
+        res.json({
+            success: "Signed up successfully",
+        });
+
+    } catch (err) {
+        return res.status(500).json({
+            message: "Something went wrong",
+            error: err.message,
+        });
+    }
 });
+
 
 // signin
-userRouter.post('/signin', (req, res) => {
-    res.json({
-        message: "sign endpoint"
-    });
+userRouter.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
 
+    if (!email || !password) {
+        res.status(401).json({
+            message: "Username or Password can't be empty",
+        });
+    }
+
+    try {
+        const user = await userModel.findOne({ email });
+
+        if (!user) {
+            res.status(401).json({
+                message: "Invalid credentials",
+            });
+        }
+
+        bcrypt.compare(password, user.password, function (err, result) {
+            if (!result) {
+                res.status(401).json({
+                    message: "Wrong Password",
+                });
+            } else {
+                let token = jwt.sign({
+                    id: user._id,
+                }, process.env.JWT_USER_SECRET);
+
+                res.setHeader('token', token);
+
+                res.json({
+                    token
+                });
+            }
+        });
+
+    } catch (error) {
+        res.status(401).json({
+            message: "Something went Wrong"
+        })
+    }
 });
+
 
 // all courses purchases
 userRouter.get('/purchases', (req, res) => {
